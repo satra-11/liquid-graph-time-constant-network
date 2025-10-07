541c5b9f36fc7ae32914cbbd732650c8485848f0
satra-11 <syuzuki546@gmail.com>
2025-10-07 16:31:10 +0900

    lint

--- scripts/train_driving.py
+++ scripts/train_driving.py
@@ -175,7 +175,6 @@ def main():
     # データローダー作成
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
     
     # モデル作成
     print("Creating models...")
