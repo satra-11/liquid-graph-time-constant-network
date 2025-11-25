dc0d4a7028ff6ceb6e206782de7a8bff33f11138
satra-11 <syuzuki546@gmail.com>
2025-11-25 14:20:35 +0900

    perf: change Dataload option

--- src/driving/data.py
+++ src/driving/data.py
@@ -29,8 +29,26 @@ def setup_dataloaders(
     )
 
     # データローダー作成
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        num_workers=16,
+        pin_memory=True,
+        shuffle=True,
+    )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        num_workers=16,
+        pin_memory=True,
+        shuffle=False,
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        num_workers=16,
+        pin_memory=True,
+        shuffle=False,
+    )
 
     return train_loader, val_loader, test_loader, full_dataset
